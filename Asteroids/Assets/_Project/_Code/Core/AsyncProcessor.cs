875cd363bb7b7544000a80b7aa38d7f8dd95dd6a
kritikall177 <max.lysickov2013@gmail.com>
2024-12-17 10:32:39 +0300

    удалил не нужный OnDestroy

--- Asteroids/Assets/_Project/_Code/Core/AsyncProcessor.cs
+++ Asteroids/Assets/_Project/_Code/Core/AsyncProcessor.cs
@@ -4,9 +4,5 @@ namespace _Project._Code.Core
 {
     public class AsyncProcessor : MonoBehaviour
     {
-        private void OnDestroy()
-        {
-            StopAllCoroutines();
-        }
     }
 }
